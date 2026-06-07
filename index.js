abdaa2dbfa27f696a404bc5c0e38343747b6ef5c
salmanibneyrahman <darazbks@gmail.com>
2026-06-08 00:52:51 +0600

    mongodb connetion fixed

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const dns = require("node:dns").promises;
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ObjectId } = require("mongodb");
